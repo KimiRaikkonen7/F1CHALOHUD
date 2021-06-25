394c3797442d9ed0f6446278a491c3a9d7db0f65
KimiRaikkonen7 <37035635+KimiRaikkonen7@users.noreply.github.com>
2021-06-26 00:06:08 +0700

    Add files via upload

---
+++ CMT/Click here to install HUD.bat
@@ -0,0 +1,6 @@
+@echo off
+
+copy Generic_F1_2020_Standard.gen ..\..\CMTSeasonData\Vehicles\Generic_F1_2020_Standard.gen /y
+copy Generic_F1_2020_Alfa.gen ..\..\CMTSeasonData\Vehicles\Generic_F1_2020_Alfa.gen /y
+copy Generic_F1_2020_RPStr.gen ..\..\CMTSeasonData\Vehicles\Generic_F1_2020_RPStr.gen /y
+copy Generic_F1_2020_RPPer.gen ..\..\CMTSeasonData\Vehicles\Generic_F1_2020_RPPer.gen /y
